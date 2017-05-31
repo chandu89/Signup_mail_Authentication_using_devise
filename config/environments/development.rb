450b0de88dd71f6e7550b63430afe7e63560dfff
chandu89 <chandanwtb2009@gmail.com>
2017-05-31 16:08:15 +0530

    Second commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,14 +42,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      'chandanwtb2009@gmail.com',
-    password:       'twgenearjwsizwkk',
-    domain:         'localhost:3000',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  
+  
 end
