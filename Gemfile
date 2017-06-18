986291e669a32a980976969a93e23f1da8fff378
chandu89 <chandanwtb2009@gmail.com>
2017-06-18 13:55:05 +0530

    Added Boostrap 3 & Addition parameter in user

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 
 gem 'devise'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531093625) do
+ActiveRecord::Schema.define(version: 20170618081904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20170531093625) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
